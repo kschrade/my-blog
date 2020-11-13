db94f8f5176f6eb309ec89b3d0bc8661b949da62
kschrade <kyleschrade2@gmail.com>
2020-11-12 21:57:18 -0500

    adding logo to bottom

--- src/components/Post/Author/Author.js
+++ src/components/Post/Author/Author.js
@@ -1,19 +1,21 @@
 // @flow strict
-import React from 'react';
-import { getContactHref } from '../../../utils';
-import styles from './Author.module.scss';
-import { useSiteMetadata } from '../../../hooks';
+import React from "react";
+import { getContactHref } from "../../../utils";
+import styles from "./Author.module.scss";
+import { useSiteMetadata } from "../../../hooks";
+import logo from "../../images/logo-small-colored-no-background.png";
 
 const Author = () => {
   const { author } = useSiteMetadata();
 
   return (
-    <div className={styles['author']}>
-      <p className={styles['author__bio']}>
+    <div className={styles["author"]}>
+      <img src={logo} alt="Logo" className={styles["logo"]} />
+      <p className={styles["author__bio"]}>
         {author.bio}
         <a
-          className={styles['author__bio-twitter']}
-          href={getContactHref('twitter', author.contacts.twitter)}
+          className={styles["author__bio-twitter"]}
+          href={getContactHref("twitter", author.contacts.twitter)}
           rel="noopener noreferrer"
           target="_blank"
         >
